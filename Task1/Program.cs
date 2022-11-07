086b60b438da6a43aefc17ec04d82a8ae84ec125
IrinaTsagaeva <irina-hadaeva@yandex.ru>
2022-11-07 21:11:49 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿bool Palindrome (int number)
+{
+    int reverse_num = 0;
+    int front_num = number;
+    while (number>0)
+    {
+        reverse_num = reverse_num*10 + number%10;
+        number = number/10;
+    }
+    if (reverse_num==front_num)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+Console.Clear();
+Console.WriteLine("Пожалуйста, введите Ваше число!");
+bool text = Int32.TryParse(Console.ReadLine(), out int number);
+if (text)
+{
+    if (Palindrome(number))
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Введенный текст не является числом!!!");
+}
