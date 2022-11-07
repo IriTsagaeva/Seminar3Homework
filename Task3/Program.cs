ce32ca59f79bb8d286b1f9b48f7c0b2a70a9963a
IrinaTsagaeva <irina-hadaeva@yandex.ru>
2022-11-07 21:56:02 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿void PrintCubeTable (int number)
+{
+    int count = 1;
+    while (count<=number)
+    {
+        Console.Write(Math.Pow(count,3)+" ");
+        count++;
+    }
+}
+
+
+Console.WriteLine("Please, enter your number");
+bool text = Int32.TryParse(Console.ReadLine(), out int number);
+if (text)
+{
+    PrintCubeTable(number);
+}
+else
+{
+    Console.Write("Uncorrect number!!!!");
+}
